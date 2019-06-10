6a996f2c60bf7f2835ec48548e5f9caa9fb8ac25
Supreeth <supreeth.b@outlook.com>
2019-06-10 22:51:12 +0530

    Added local files

---
+++ src/main/java/com/supreeth/chat/CrossTalkV2Application.java
@@ -0,0 +1,13 @@
+package com.supreeth.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossTalkV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossTalkV2Application.class, args);
+	}
+
+}
